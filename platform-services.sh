caa84c2af9f89a07088cdaaa1b1f3a286edc97b1
AnthonyAmanse <joe.amanse@obf.ateneo.edu>
2017-04-28 10:48:25 -0700

    removed core and platform scripts

--- platform-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl create -f platform/controller.yaml
-kubectl create -f platform/couchdb.yaml
-kubectl create -f platform/kafka.yaml
-kubectl create -f platform/redis.yaml
-kubectl create -f platform/registry.yaml
